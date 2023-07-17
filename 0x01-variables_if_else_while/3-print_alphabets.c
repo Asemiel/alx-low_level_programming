4e76fb0cdb7c6191f34f4ebf6a929cbc5c927b95
Asemiel <temitayoasemah@gmail.com>
2023-07-17 12:08:58 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,22 +6,13 @@
 
 int main(void)
 {
-	char c;
+	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
 
-	char d;
-
-	c = 'a';
-	d = 'd';
-	while
-		(c <= 'z') {
-			putchar(c);
-			c++;
-		}
-	while
-		(c <= 'z') {
-			putchar(d);
-			d++;
-		}
+	for (i = 0; i < 52; i++)
+	{
+		putchar(alp[i]);
+	}
 	putchar('\n');
 	return (0);
 }
