b787545e4f5d48b9680641b0e18390d5a1f2c714
Asemiel <temitayoasemah@gmail.com>
2023-07-17 07:21:27 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,7 +2,7 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main - Entry point 
+ * main - This program will assign a random number to the variable.
  * Return: Always return 0 (success)
  */
 int main(void)
@@ -10,14 +10,16 @@ int main(void)
 	int n;
 
 	int m;
+
 	srand(time(0));
+
 	n = rand() - RAND_MAX / 2;
-	m = n%10;
+	m = n % 10;
 	if (m > 5)
-		printf("Last digit of %d is %d and is greater than 5\n", n,m);
+		printf("Last digit of %d is %d and is greater than 5\n", n, m);
 	if (m == 0)
-		printf("Last digit of %d is %d and is 0\n", n,m);
+		printf("Last digit of %d is %d and is 0\n", n, m);
 	if (m < 6 && m != 0)
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n,m);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, m);
 	return (0);
 }
