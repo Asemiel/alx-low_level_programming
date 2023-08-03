a8692e05a2194d6b6f01981e70a402a11442b1f2
Asemiel <temitayoasemah@gmail.com>
2023-08-03 05:29:22 -0700

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -2,12 +2,17 @@
 /**
  * _print_rev_recursion - prints a string in reverse
  * @s: The string to be printed.
+ * Return: nothing.
  */
 void _print_rev_recursion(char *s)
 {
-if (*s)
+if (*s == '\0')
 {
-_print_rev_recursion(s + 1);
+return;
+}
+s++;
+_print_rev_recursion(s);
+s--;
 -putchar(*s);
 }
 }
