8f5456c73bc17a0e841d317310493c57b2a8806d
Asemiel <temitayoasemah@gmail.com>
2023-08-07 07:24:12 -0700

    ./mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 /**
  * main - prints all arguments it receives
- * _atoi - This converts a string to an integer
+ * atoi - This converts a string to an integer
  * @argc: number of arguments
  * @argv: array of arguments
  *
@@ -10,17 +10,18 @@
  */
 int main(int argc, char *argv[])
 {
-int (result = 0), num1, num2;
+int result;
+int num1;
+int num2;
 if (argc != 3)
 {
 printf("%s\n", "Error");
 return (1);
 }
-num1 = _atoi(argv[1]);
-num2 = _atoi(argv[2]);
-result = num1 * num2;
+num1 = atoi(argv[1]);
+num2 = atoi(argv[2]);
+result = num1 *num2;
 
 printf("%d\n", result);
 return (0);
 }
-
