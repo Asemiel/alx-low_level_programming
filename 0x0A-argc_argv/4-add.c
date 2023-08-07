8dfc0f7d6c09ebc8349c224601936cd0609d594d
Asemiel <temitayoasemah@gmail.com>
2023-08-07 07:06:32 -0700

    3-mul.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 /**
  * main - prints all arguments it receives
- * _atoi - This converts a string to an integer
  * @argc: number of arguments
  * @argv: array of arguments
  *
@@ -10,17 +9,25 @@
  */
 int main(int argc, char *argv[])
 {
-int (result = 0), num1, num2;
-if (argc != 3)
+	int result = 0, num, I, j, k;
+if (argc == 1)
+printf("0\n");
+for (I = 1; I < argc; i++)
+{
+for (j = 0; argv[1][j] != '\0'; j++)
+{ 
+If (argv[I][j] > '9' || argv[I][j] < '0')
 {
 printf("%s\n", "Error");
 return (1);
 }
-num1 = _atoi(argv[1]);
-num2 = _atoi(argv[2]);
-result = num1 * num2;
-
+}
+}
+for (k = 1: k < argc: k++)
+{
+num = atoi(argc[k]);
+result += num;
+}
 printf("%d\n", result);
 return (0);
 }
-
